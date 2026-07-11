4f9c5d64780e49c1ff973fdccb5e544dad7eafbd
Eugenio - Home <eugenio.socha2004@gmail.com>
2026-07-11 19:28:20 -0400

    feat: Added all dtos and service for Categoria

--- Application/DTOs/Categoria/CategoriaResponseDto.cs
+++ Application/DTOs/Categoria/CategoriaResponseDto.cs
@@ -1,7 +1,8 @@
 ﻿namespace insume_backend.Application.DTOs.Categoria
 {
-    public class CategoriaRequestDto
+    public class CategoriaResponseDto
     {
+        public int Id { get; set; }
         public string Titulo { get; set; } = string.Empty;
     }
 }
